6567316733c4d5cb5ea2af52a9c5583bd1c0d37d
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-18 20:40:37 +0200

    Start loading animation on click

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,33 +6,48 @@ import Spinner from './LoadingSpinner'
 
 function App() {
 
-  const [ generated, isGenerated ] = useState(false);
+  const [ generated, setGenerated ] = useState(false);
+  const [ loading, setLoading ] = useState(false)
 
   const handleGenerateClick = () => {
-    if(!generated) isGenerated(true)
+
+    if(!generated && !loading){
+      
+      setLoading(true);
+      setTimeout(() =>{
+        setGenerated(true)
+        setLoading(false)
+      }, 3000)
+    } 
   }
   const resetIsGenerated = () => {
-    isGenerated(prev => !prev);
+    setGenerated(prev => !prev);
   }
   
   return (
     <>
       <div className='main'> 
         <div className='generate-div'>
-          <SingleButton vartiation='generate' onClick={handleGenerateClick} color={generated ? 'color' : ''} disabled={generated}>Generate</SingleButton>
+          <SingleButton vartiation='generate' onClick={handleGenerateClick} color={generated ? 'color' : ''} disabled={generated || loading}>Generate</SingleButton>
         </div>
         <div className='qr-code-content'>
-          {generated ? <div className='qr-code-scanner' style={{backgroundColor: 'green'}}>
+          {loading ? <Spinner/> 
+          : 
+          <>{generated ? <div className='qr-code-scanner' style={{backgroundColor: 'green'}}>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-qrcode"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 17l0 .01" /><path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 7l0 .01" /><path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M17 7l0 .01" /><path d="M14 14l3 0" /><path d="M20 14l0 .01" /><path d="M14 14l0 3" /><path d="M14 20l3 0" /><path d="M17 17l3 0" /><path d="M20 17l0 3" /></svg>
-            <SingleButton vartiation='generate-qr-code' color='color' disabled={generated}>Generate</SingleButton>
+            <SingleButton vartiation='generate-qr-code' color='color' disabled={generated || loading}>Generate</SingleButton>
           </div> 
           : 
           <div className='qr-code-scanner'>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-qrcode"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 17l0 .01" /><path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 7l0 .01" /><path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M17 7l0 .01" /><path d="M14 14l3 0" /><path d="M20 14l0 .01" /><path d="M14 14l0 3" /><path d="M14 20l3 0" /><path d="M17 17l3 0" /><path d="M20 17l0 3" /></svg>
-            <SingleButton vartiation='generate-qr-code' onClick={() => {isGenerated(prev => !prev)}}>Generate</SingleButton>
+            <SingleButton vartiation='generate-qr-code' onClick={() => {
+              setGenerated(prev => !prev)
+              handleGenerateClick()
+            }}>Generate</SingleButton>
           </div>}
           {generated ? <CountDownTimmer resetIsGenerated={resetIsGenerated}/> : <h1>0s</h1>}
-          {generated ? <SingleButton vartiation='download'>Download</SingleButton> : <SingleButton vartiation='download'  color='color'>Download</SingleButton>}
+          {generated ? <SingleButton vartiation='download'>Download</SingleButton> : <SingleButton vartiation='download'  color='color'>Download</SingleButton>}</>}
+          
         </div>
       </div>
     </>
--- my-app/src/LoadingSpinner.jsx
+++ my-app/src/LoadingSpinner.jsx
@@ -1,6 +1,13 @@
 
-const Spinner = () => {
+const Spinner = ({ size = 'medium' }) => {
 
+    return(
+        <img 
+        src="./Spinner@1x-1.0s-200px-200px.gif" 
+        alt="Loading..." 
+        style={{ width: size, height: size }} 
+    />
+    )
 }
 
 export default Spinner;
