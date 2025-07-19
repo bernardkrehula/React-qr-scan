897e1b365401d2561f8250b1131e135e58cb4df3
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-20 00:10:21 +0200

    Fix click on generate btn

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,16 +8,7 @@ function App() {
   const [ loading, setLoading ] = useState(false)
   const [ time, setTime ] = useState(5);
 
-  const handleGenerateClick = async() => {
-    
-    if(!generated && !loading){
-
-      await new Promise(resolve => setTimeout(resolve, 2000))
-      setLoading(false)
-    } 
-  }
-
-  const timming = () => {
+  const handleGenerateClick = () => {
     setTime(5);
   }
   
@@ -53,10 +44,7 @@ function App() {
     <>
       <div className='main'> 
         <div className='generate-div'>
-          <SingleButton onClick={async() => {
-              await handleGenerateClick()
-              timming()
-            }} disabled={generated || loading}>Generate</SingleButton>
+          <SingleButton onClick={async() => {handleGenerateClick()}} disabled={generated || loading}>Generate</SingleButton>
         </div>
         <div className='qr-code-content'>
           {loading ? <Spinner/> 
@@ -64,10 +52,7 @@ function App() {
           <>
           <div className='qr-code-scanner' aria-disabled={generated}>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-qrcode"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 17l0 .01" /><path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M7 7l0 .01" /><path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M17 7l0 .01" /><path d="M14 14l3 0" /><path d="M20 14l0 .01" /><path d="M14 14l0 3" /><path d="M14 20l3 0" /><path d="M17 17l3 0" /><path d="M20 17l0 3" /></svg>
-            <SingleButton onClick={async() => {
-              await handleGenerateClick()
-              timming()
-            }} disabled={generated}>Generate</SingleButton>
+            <SingleButton onClick={() => {handleGenerateClick()}} disabled={generated}>Generate</SingleButton>
           </div>
           <div className='countdown-timer'>{time}s</div>
           <SingleButton disabled={!generated}>Download</SingleButton>
